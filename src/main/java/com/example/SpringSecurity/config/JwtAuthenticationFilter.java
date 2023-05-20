7d3647d4cc0e195079320bee383eb576b0dba646
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-20 19:24:05 +0300

    OncePerRequestFilter

--- src/main/java/com/example/SpringSecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/SpringSecurity/config/JwtAuthenticationFilter.java
@@ -2,20 +2,33 @@ package com.example.SpringSecurity.config;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.micrometer.common.lang.NonNull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
+
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request, // our request
+			@NonNull HttpServletResponse response, // our response
+			@NonNull FilterChain filterChain // contains list of other filters we need
+	) throws ServletException, IOException {
+
+		final String authHeader = request.getHeader("Authorisation"); // it contains the token
+		final String Jwt;
 		
+		if()
+	
 	}
 
 }
