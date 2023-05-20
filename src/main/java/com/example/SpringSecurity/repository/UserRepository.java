663dcfc29af18be4febc6195be665e6428676ce1
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-20 19:13:09 +0300

    JwtAuthenticationFilter

--- src/main/java/com/example/SpringSecurity/repository/UserRepository.java
+++ src/main/java/com/example/SpringSecurity/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.SpringSecurity.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.SpringSecurity.model.User;
 
+@Repository
 public interface UserRepository  extends JpaRepository<User,Integer>{
 	
 	Optional<User> findByEmail(String Email);
