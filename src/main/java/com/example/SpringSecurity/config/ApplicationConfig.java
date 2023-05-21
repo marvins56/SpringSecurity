cb67c3eb19fc87fd51a4a19aa17120df19ee84ac
Kauta Marvin <82571414+marvins56@users.noreply.github.com>
2023-05-21 17:04:54 +0300

    authenticationProvider

--- src/main/java/com/example/SpringSecurity/config/ApplicationConfig.java
+++ src/main/java/com/example/SpringSecurity/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.example.SpringSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -19,6 +20,12 @@ public class ApplicationConfig {
 	public UserDetailsService  userDetailsService() {
 		return username -> userRepository.findByEmail(username)
 				.orElseThrow(()-> new UsernameNotFoundException("user Not Found"));
+		
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		//FETCHES DATA AND ENCODE PASSOWRDS ETC
 		
 	}
 }
